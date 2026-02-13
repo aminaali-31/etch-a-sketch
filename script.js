1be0be6661c1199e23c3ed557ed4a6a750caf1cd
Amna <amina.ali.31032008@gmail.com>
2026-02-13 17:22:48 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Hello I have been loaded");
+let cont = document.querySelector('.container')
+let btn = document.querySelector('button')
+btn.addEventListener('click', () => askSize())
+let n = 16;
+createGrid(n*n ,16);
+function askSize()
+{
+    let size = prompt("Enter no. of squares per side: ")
+    if (size < 1 || size > 125)
+    {
+        alert("Cannot create a grid of this size")
+        return;
+    }
+    else {
+        createGrid(size * size , size);
+    }
+}
+function createGrid(siz,size)
+{
+    cont.innerHTML = '';
+    for (let i = 1; i <= siz; i++)
+    {
+    let div = document.createElement('div');
+    div.style.setProperty('--cols',size);
+    div.style.backgroundColor = 'rgb(255, 255, 255)';
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r},${g},${b})`;
+    div.addEventListener('mouseover', () => div.style.backgroundColor = color)
+    cont.appendChild(div);
+    }
+}
